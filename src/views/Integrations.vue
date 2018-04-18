50babca269fd6e95dc286550e739898af642cc96
Jason Priem <jp@jasonpriem.org>
2018-04-18 11:19:14 -0400

    words.

--- src/views/Integrations.vue
+++ src/views/Integrations.vue
@@ -2,7 +2,10 @@
     <div class="page integrations">
         <h1>Integrations</h1>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel ornare orci. Nam congue sollicitudin nibh. Nulla ac sollicitudin mi. Pellentesque libero metus, interdum sed dolor in, tincidunt interdum urna.
+            <em>This page is still under construction...</em>
+        </p>
+        <p>
+            Although we offer many products to help folks access Unpaywall data, when it comes down to it we're just one of the users of our own dataset. There are a lot of great integrations from others that can help you get more out of the data than we can. Here's a partial list:
         </p>
 
         <h2 id="search">Search</h2>
--- src/views/products/Extension.vue
+++ src/views/products/Extension.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="page product-page extension">
         <h1>Browser Extension</h1>
+        <p>
+            Our browser extension uses the Unpaywall database to find Open Access versions of paywalled scholarly articles as you browse. It's available for Chrome and Firefox, and is currently installed by about 130,000 users.
+        </p>
+        <p>
+            The extension has its own website with more information <a href="https://unpaywall.org">here.</a>
+        </p>
     </div>
 </template>
 
