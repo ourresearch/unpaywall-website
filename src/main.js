8307e800acadee5e3f2f79dbcdb824474f277c0e
Jason Priem <jp@jasonpriem.org>
2018-04-24 13:07:34 -0700

    add google analytics.

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import VueScrollTo from 'vue-scrollto'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(VueMaterial)
 Vue.use(VueScrollTo)
-
+Vue.use(VueAnalytics, {
+  id: "UA-23384030-6",
+  router
+})
 
 
 Vue.config.productionTip = false
