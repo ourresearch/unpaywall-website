2807cf32b352f33d4ce5e14e60e270c6a5e0b208
Heather Piwowar <hpiwowar@gmail.com>
2018-07-04 18:06:44 -0700

    correct "versions" to "version" in api docs, and update the latest snapshot date

--- src/views/products/Snapshot.vue
+++ src/views/products/Snapshot.vue
@@ -14,7 +14,7 @@
             Versions
         </h2>
         <p>
-            We release a new snapshot about twice a year. If you want more up-to-date data, it works well to pair the snapshot with ongoing diffs, available via our <router-link to="/products/api">Data Feed</router-link> product. The most recent version was released April 18, 2018.
+            We release a new snapshot about twice a year. The most recent version was released June 21, 2018.  If you want more up-to-date data, it works well to pair the snapshot with ongoing diffs, available via our <router-link to="/products/api">Data Feed</router-link> product.
         </p>
 
 
