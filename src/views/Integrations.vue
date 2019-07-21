0e319a97af6d799a8ed0a62e8ed2d3ff2f71cab1
Jason Priem <jp@jasonpriem.org>
2019-07-20 21:24:22 -0700

    much more complete integration page.

--- src/views/Integrations.vue
+++ src/views/Integrations.vue
@@ -1,33 +1,126 @@
 <template>
     <div class="page integrations">
         <h1>Integrations</h1>
-        <p>
-            Although we offer many products to help folks access Unpaywall data, when it comes down to it we're just one of the users of our own dataset. There are a lot of great integrations from others that can help you get more out of the data than we can. Here's a partial list:
-        </p>
+        <p>The best part about Open Data is all the awesome uses it can support downstream. We're thrilled that there are lots of ways to access and use the Unpaywall dataset, thanks to our partners and integrations:</p>
 
-        <h2 id="search">Search</h2>
+
+
+        <h2 id="search">Citation indices</h2>
         <ul>
-            <li> <a href="https://app.dimensions.ai/discover/publication?and_facet_open_access=True">Dimensions</a> </li>
+            <li><a href="https://app.dimensions.ai/discover/publication?and_facet_open_access=True">Dimensions</a></li>
             <li><a href="https://blog.impactstory.org/elsevier-data-feed/">Scopus</a></li>
-            <li><a href="https://clarivate.com/wp-content/uploads/2017/10/Clarivate-OA-Impact-Story_Intro-sheet.pdf?elqTrackId=c5abee7048934a3da317e296a3e2a9c8&elqaid=5350&elqat=2">Web of Science</a></li>
+            <li>
+                <a href="https://clarivate.com/wp-content/uploads/2017/10/Clarivate-OA-Impact-Story_Intro-sheet.pdf?elqTrackId=c5abee7048934a3da317e296a3e2a9c8&elqaid=5350&elqat=2">Web
+                    of Science</a></li>
         </ul>
+
+
+        <h2 id="discover">Discovery tools</h2>
+        <ul>
+            <li><a href="https://cloud.ebsco.com/apps/unpaywall">EBSCO Discovery System (EDS)</a></li>
+            <li><a href="http://blog.europepmc.org/2018/04/unlocking-open-europe-pmc-integrates.html">Europe PubMed Central</a></li>
+            <li><a href="https://gettheresearch.org">Get The Research</a></li>
+            <li><a href="https://www.meta.org/">Meta</a> (Chan Zuckerberg Institute)</li>
+            <li>
+                ProQuest <a href="https://knowledge.exlibrisgroup.com/Primo/Community_Knowledge/How_to_%E2%80%93_Add_an_Unpaywall_General_Electronic_Service_to_Alma%2C_for_Open_Access_links_in_Primo">Alma, </a>
+                <a href="https://developers.exlibrisgroup.com/appcenter/unpaywall/">Primo, </a> and
+                <a href="https://ideas.exlibrisgroup.com/forums/564566-summon/suggestions/36261826-add-unpaywall-database-to-the-summon-index">Summon (launching soon)</a>
+            </li>
+            <li><a href="https://help.oclc.org/Discovery_and_Reference/WorldCat_Discovery/Search_results/Open_content_in_WorldCat_Discovery_search_results#Open_Access_DOI_links_enhanced_by_Unpaywall">OCLC WorldCat Discovery</a></li>
+            <li><a href="https://blog.scienceopen.com/category/aggregation/">ScienceOpen</a></li>
+
+
+            <li><a href="https://tdnet.io/services/discovery">TDNet</a></li>
+            <li><a href="https://www.tripdatabase.com/">Trip Database</a></li>
+
+
+        </ul>
+
+
 
         <h2 id="link-resolvers">Library link resolvers</h2>
         <ul>
-            <li><a href="https://knowledge.exlibrisgroup.com/SFX/Knowledge_Articles/How_to_enable_oaDOI_service_on_SFX">SFX</a></li>
-            <li><a href="https://knowledge.exlibrisgroup.com/360_Services/360_Link/0Product_Documentation/Overview/360_Link_with_IEDL%3A_Open_Access_Lookup_Service_Integration">360 Link,</a></li>
-            <li><a href="https://developers.exlibrisgroup.com/blog/Adding-an-oadoi-dot-org-GES-link-to-Primo">Primo</a></li>
-            <li>Roll your own: the shortcut link <code>https://unpaywall.org/YOUR_DOI</code> automatically redirects to an open version of <code>YOUR_DOI</code> if we have one; many libraries have added this as a manual target in their own link resolvers.</li>
+            <li>
+                <a href="https://knowledge.exlibrisgroup.com/360_Services/360_Link/0Product_Documentation/Overview/360_Link_with_IEDL%3A_Open_Access_Lookup_Service_Integration">360
+                    Link</a>
+            </li>
+            <li><a href="https://knowledge.exlibrisgroup.com/SFX/Knowledge_Articles/How_to_enable_oaDOI_service_on_SFX">SFX</a>
+            </li>
+            <li>Roll your own: the shortcut link <code>https://unpaywall.org/YOUR_DOI</code> automatically redirects to
+                an open version of <code>YOUR_DOI</code> if we have one; many libraries have added this as a manual
+                target in their own link resolvers.
+            </li>
 
         </ul>
+
+
+
+
+        <h2>Usage in Funding Analysis</h2>
+        <p>These funders have are all using Unpaywall to help track the openness of the research they fund, and improve their efforts to promote Open Access:</p>
+        <ul>
+            <li>US National Institutes of Health</li>
+            <li>UK REF 2021 (Research Excellence Framework)</li>
+            <li>French Ministry of Higher Education & Research</li>
+            <li>Swiss National Science Foundation</li>
+            <li>German Federal Ministry of Education and Research</li>
+            <li>The Wellcome Trust</li>
+        </ul>
+
+
+
+
+        <h2>Online OA Dashboards</h2>
+        <ul>
+            <li><a href="https://najkoja.shinyapps.io/hybridoa/">Hybrid OA Monitor</a></li>
+            <li>
+                <a href="https://ec.europa.eu/info/research-and-innovation/strategy/goals-research-and-innovation-policy/open-science/open-science-monitor/trends-open-access-publications_en">European Open Science Monitor</a></li>
+            <li><a href="https://open-access-monitor.de/#/publications">German Open Access Monitor</a></li>
+        </ul>
+
+
+        <h2>Browser extensions</h2>
+        <ul>
+            <li><a href="https://kopernio.com/">Kopernio</a></li>
+            <li><a href="https://www.leanlibrary.com/">Lean Library</a></li>
+            <li><a href="https://openaccessbutton.org/">Open Access Button</a></li>
+            <li>
+                <a href="https://chrome.google.com/webstore/detail/unpaywall/iplffkdpngmdjhlpjmppncnlhomiipha?hl=en">Unpaywall browser extension</a></li>
+        </ul>
+
+
+
+
+
+
+
+        <h2>Machine learning and AI</h2>
+        <ul>
+            <li><a href="https://www.facten.com/">Facten</a></li>
+            <li><a href="https://www.nature.com/articles/d41586-019-02142-1">Jawaharlal Nehru University Data Depot</a></li>
+            <li><a href="http://machineintelligence.se/">Machine Intelligence Sweden</a></li>
+            <li><a href="https://mitpress.mit.edu/kfg">MIT Knowledge Futures Group</a></li>
+            <li><a href="https://researchably.com/">Researchably</a></li>
+            <li><a href="https://www.scholarcy.com/">Scholarcy</a></li>
+            <li><a href="https://www.semanticscholar.org/">Semantic Scholar</a></li>
+            <li><a href="https://www.wizdom.ai/">Wizdom.ai</a></li>
+        </ul>
+
+
 
 
         <h2 id="other">Other integrations</h2>
         <ul>
+            <li><a href="https://zenodo.org/record/1441072#.XTPinZNKjUI">OpenAIRE DOIBoost</a></li>
+
             <li>
                 <a href="https://cran.r-project.org/web/packages/roadoi/vignettes/intro.html">R client for our API</a>
             </li>
+            <li><a href="https://www.zotero.org/blog/improved-pdf-retrieval-with-unpaywall-integration/">Zotero</a></li>
+            <li>
+                <a href="https://blog.openaccessbutton.org/new-powerfully-simple-library-tool-to-deliver-articles-no-subscription-needed-1c8b7def604f">InstantILL</a> (Interlibrary loan software from Open Access Button)</li>
         </ul>
+
 
     </div>
 </template>
@@ -37,9 +130,20 @@
         name: "Integrations",
         metaInfo: {
             title: "Integrations"
-        }    }
+        }
+    }
 </script>
 
 <style scoped>
+    h2 {
+        margin-bottom: 0;
+        padding-bottom: 0;
+    }
+    ul {
+        margin-top: 0;
+    }
+    p {
+        margin-top: 0;
+    }
 
 </style>
