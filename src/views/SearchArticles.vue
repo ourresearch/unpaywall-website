18a2f2dccf0369e71703713c4008439cdd45a677
Jason Priem <jp@jasonpriem.org>
2020-08-18 16:37:26 -0700

    update packages.

--- src/views/SearchArticles.vue
+++ src/views/SearchArticles.vue
@@ -1,10 +1,13 @@
 <template>
-    <v-container>
-        <h1>Articles</h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-    </v-container>
+    <div class="page">
+        <v-container>
+            <h1>Articles</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </v-container>
+
+    </div>
 </template>
 
 <script>
@@ -48,6 +51,10 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    div.page {
+        max-width: 100% !important;
+    }
+
 
 </style>
