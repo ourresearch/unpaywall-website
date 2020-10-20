98c640a2cbf4343d7c9f16fa17fe66707703087e
Richard Orr <richard@impactstory.org>
2020-10-20 12:52:32 -0400

    update github url on contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,7 +4,7 @@
         <p>Thanks for your interest in getting in touch! We'd love to hear from you. Please drop us a line at <a href="mailto:support@unpaywall.org">support@unpaywall.org</a> to report bugs, and at <a href="mailto:team@ourresearch.org">team@ourresearch.org</a> for everything else.</p>
         <p>If you want to keep up with our goings-on, there are a few more options. You can follow us on Twitter at <a href="https://twitter.com/our_research">@our_research</a> and <a href="https://twitter.com/unpaywall">@unpaywall,</a> subscribe to the
             <a href="https://groups.google.com/forum/#!forum/unpaywall">Unpaywall mailing list, </a>  or follow us on
-            <a href="https://github.com/Impactstory">GitHub.</a>
+            <a href="https://github.com/ourresearch">GitHub.</a>
         </p>
     </div>
 </template>
