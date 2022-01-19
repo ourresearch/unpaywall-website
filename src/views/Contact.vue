a2266c031c14c616f44bd77c3fa3fa4aca02062a
Richard Orr <richard@ourresearch.org>
2022-01-19 11:03:57 -0500

    fix twitter handle

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,7 +2,7 @@
     <div class="page contact">
         <h1>Contact</h1>
         <p>Thanks for your interest in getting in touch! We'd love to hear from you. Please drop us a line at <a href="mailto:support@unpaywall.org">support@unpaywall.org</a> to report bugs, and at <a href="mailto:team@ourresearch.org">team@ourresearch.org</a> for everything else.</p>
-        <p>If you want to keep up with our goings-on, there are a few more options. You can follow us on Twitter at <a href="https://twitter.com/our_research">@our_research</a> and <a href="https://twitter.com/unpaywall">@unpaywall,</a> subscribe to the
+        <p>If you want to keep up with our goings-on, there are a few more options. You can follow us on Twitter at <a href="https://twitter.com/OurResearch_org">@OurResearch_org</a> and <a href="https://twitter.com/unpaywall">@unpaywall,</a> subscribe to the
             <a href="https://groups.google.com/forum/#!forum/unpaywall">Unpaywall mailing list, </a>  or follow us on
             <a href="https://github.com/ourresearch">GitHub.</a>
         </p>
